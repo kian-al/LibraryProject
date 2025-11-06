d1f77664d9c210eabe4846c69639aed3027fedb8
Kian <122436242+kian-al@users.noreply.github.com>
2025-11-07 00:13:11 +0330

    feat(controller): create UserController with REST endpoints

---
+++ src/main/java/ir/library/libraryproject/controller/UserController.java
@@ -0,0 +1,27 @@
+package ir.library.libraryproject.controller;
+
+import ir.library.libraryproject.Model.User;
+import ir.library.libraryproject.Service.User.UserService;
+import ir.library.libraryproject.dto.response.UserResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService=userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> save(@RequestBody User user){
+        return ResponseEntity.ok(userService.save(user));
+
+    }
+}
