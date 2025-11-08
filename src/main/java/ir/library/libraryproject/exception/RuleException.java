92e9a8f3582d9e51f12724fb0f8fd0caf50e1aa0
Kian <122436242+kian-al@users.noreply.github.com>
2025-11-08 17:28:49 +0330

    refactor: rename package structure for naming consistency

--- src/main/java/ir/library/libraryproject/exception/RuleException.java
+++ src/main/java/ir/library/libraryproject/exception/RuleException.java
@@ -1,4 +1,4 @@
-package ir.library.libraryproject.execption;
+package ir.library.libraryproject.exception;
 
 public class RuleException extends RuntimeException {
     public RuleException(String message) {
