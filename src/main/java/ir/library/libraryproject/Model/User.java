0533390f23488bc6629e35d7cb5453992ca42251
Kian <122436242+kian-al@users.noreply.github.com>
2025-11-16 18:01:13 +0330

    feat: add @AllArgsConstructor and @NoArgsConstructor for Builder needed annotations

--- src/main/java/ir/library/libraryproject/Model/User.java
+++ src/main/java/ir/library/libraryproject/Model/User.java
@@ -2,13 +2,15 @@ package ir.library.libraryproject.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @Table(schema = SchemaName.SchemaName)
+@Builder
+@AllArgsConstructor //for @Builder needed
+@NoArgsConstructor //for hibernate needed
 public class User extends BaseEntity{
 
     private String username;
